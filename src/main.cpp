a46434d93991a7930761f972fef802a9e1f9e93b
Miyuki Ito <miyuki.ito@slurp.coffee>
2024-11-05 23:03:31 +0200

    read map and draw map with color in window

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 #include "SFML/Graphics/RenderWindow.hpp"
 
-std::vector<std::vector<int>> read_map(const std::string& filename);
+std::vector<std::vector<char>> read_map(const std::string& filename);
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-    read_map("map/map1.txt");
+    std::vector<std::vector<char>> map = read_map("map/map2.txt");
+    const int tileSize = 32;
+    int rows = map.size(); // マップの行数
+    int cols = map[0].size(); // マップの列数
+
+    sf::RenderWindow window(sf::VideoMode(cols * tileSize, rows * tileSize), "My window");
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -22,6 +27,29 @@ int main()
         }
 
         window.clear(sf::Color::Blue);
+
+        for (int row = 0; row < map.size(); ++row) {
+            for (int col = 0; col < map[row].size(); col++) {
+                if (map[row][col] == '1') {
+                    sf::RectangleShape wall(sf::Vector2f(tileSize, tileSize));
+                    wall.setFillColor(sf::Color::Blue);
+                    wall.setPosition(col * tileSize, row * tileSize);
+                    window.draw(wall);
+                }
+                else if (map[row][col] == '0') {
+                    sf::RectangleShape path(sf::Vector2f(tileSize, tileSize));
+                    path.setFillColor(sf::Color::White);
+                    path.setPosition(col * tileSize, row * tileSize);
+                    window.draw(path);
+                }
+                else if (map[row][col] == 'C') {
+                    sf::RectangleShape item(sf::Vector2f(tileSize, tileSize));
+                    item.setFillColor(sf::Color::Red);
+                    item.setPosition(col * tileSize, row * tileSize);
+                    window.draw(item);
+                }
+            }
+        }
         window.display();
     }
 
--- src/read_map.cpp
+++ src/read_map.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 
-std::vector<std::vector<int>> read_map(const std::string& filename) {
+std::vector<std::vector<char>> read_map(const std::string& filename) {
     std::ifstream file(filename);
 
     if(!file.is_open()) {
@@ -10,23 +10,26 @@ std::vector<std::vector<int>> read_map(const std::string& filename) {
         return {}; // return an empty vector
     }
 
-    std::vector<std::vector<int>> map_data;
+    std::vector<std::vector<char>> map_data;
     std::string line;
 
     while (std::getline(file, line)) {
-        std::vector<int> row;
+        std::vector<char> row;
+        // 範囲ベースforループの構文
         for (char c : line) {
             if (c == '1')
-                row.push_back(1);
+                row.push_back('1');
             else if (c == '0')
-                row.push_back(0);
+                row.push_back('0');
+            else if (c == 'C')
+                row.push_back('C');
         }
         map_data.push_back(row);
     }
 
     // for testing
     for (const auto& row : map_data) {
-        for (int cell : row) {
+        for (char cell : row) {
             std::cout << cell << " ";
         }
         std::cout << std::endl;
